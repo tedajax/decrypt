fdb5d62cdfcd471a67358cf2abde72760a2f0da5
Ted Dobyns <ted.ajax@gmail.com>
2020-08-10 18:53:26 -0700

    gun and pitch

--- Assets/Decrypt/Scripts/FPSMovement.cs
+++ Assets/Decrypt/Scripts/FPSMovement.cs
@@ -3,6 +3,7 @@ using UnityEngine;
 
 public class FPSMovement : MonoBehaviour
 {
+    public Transform pivotTransform;
     private CharacterController characterController;
 
     public float moveSpeed = 5.0f;
@@ -17,17 +18,50 @@ public class FPSMovement : MonoBehaviour
     void Awake()
     {
         characterController = GetComponent<CharacterController>();
+
+        IsMouseLocked = true;
+    }
+
+    public bool IsMouseLocked
+    {
+        get { return Cursor.lockState == CursorLockMode.Locked; }
+        set
+        {
+            if (value)
+            {
+                Cursor.lockState = CursorLockMode.Locked;
+                Cursor.visible = false;
+            }
+            else
+            {
+                Cursor.lockState = CursorLockMode.None;
+                Cursor.visible = true;
+            }
+        }
     }
 
     void Update()
     {
+        if (Input.GetKeyDown(KeyCode.F1) && Input.GetKey(KeyCode.LeftShift))
+        {
+            IsMouseLocked = false;
+        }
+        else if (Input.GetMouseButtonDown(1))
+        {
+            IsMouseLocked = true;
+        }
+
         Vector3 input = new Vector3(Input.GetAxis("Horizontal"), 0.0f, Input.GetAxis("Vertical"));
         input.Normalize();
 
         float mouseLookX = Input.GetAxis("Mouse X");
+        float mouseLookY = Input.GetAxis("Mouse Y");
 
-        yaw += mouseLookX * mouseLookScalar;
+        yaw += mouseLookX * mouseLookScalar * Time.deltaTime;
         transform.rotation = Quaternion.AngleAxis(yaw, Vector3.up);
+
+        pitch = Mathf.Clamp(pitch - mouseLookY * mouseLookScalar * Time.deltaTime, -89.9f, 89.9f);
+        pivotTransform.localRotation = Quaternion.AngleAxis(pitch, Vector3.right);
 
         input = transform.TransformDirection(input);
         Vector3 inputVelocity = input * moveSpeed;
