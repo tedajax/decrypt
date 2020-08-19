51891399f9d8f70d2a64762da4646e45b7d70841
Ted Dobyns <ted.ajax@gmail.com>
2020-08-19 08:34:01 -0700

    some more ai

--- Assets/Decrypt/Scripts/Game/Units/TargetPlayerAiController.cs
+++ Assets/Decrypt/Scripts/Game/Units/TargetPlayerAiController.cs
@@ -1,16 +1,18 @@
 using UnityEngine;
+using UnityEngine.AI;
 
 public class TargetPlayerAiController : DecryptComponent
 {
-    private Signal<Participant> participantSpawnedSignal;
-    private UnityEngine.AI.NavMeshAgent agent;
-    private Transform targetTransform;
+    private Signal<Participant> participantSpawnedSignal = null;
+    private Transform targetTransform = null;
+    private NavMeshPath path;
 
     protected override void Awake()
     {
         base.Awake();
 
-        agent = GetComponent<UnityEngine.AI.NavMeshAgent>();
+        // NavMeshPath constructor cannot be called in MonoBehaviour constructor
+        path = new NavMeshPath();
 
         participantSpawnedSignal = signalBus.GetSignal<Signal<Participant>>(ParticipantSystem.SignalParticipantSpawned);
         participantSpawnedSignal.AddListener(OnParticipantSpawned);
@@ -28,13 +30,19 @@ public class TargetPlayerAiController : DecryptComponent
 
     private void Update()
     {
-        if (targetTransform != null)
+        if (targetTransform == null)
         {
-            agent.SetDestination(targetTransform.position);
+            return;
         }
-        else
+
+        if (NavMesh.CalculatePath(transform.position, targetTransform.position, 0x7FFFFFFF, path))
         {
-            agent.SetDestination(transform.position);
+            for (int i = 0; i < path.corners.Length - 1; ++i)
+            {
+                Vector3 a = path.corners[i];
+                Vector3 b = path.corners[i + 1];
+                Debug.DrawLine(a, b, Color.yellow, 0.1f);
+            }
         }
     }
 }
